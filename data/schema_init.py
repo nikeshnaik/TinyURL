a7e7565b6c5fe61b1cb6c9009c740cb0601c135e
nikeshnaik <nikesh.naik@hypersonix.io>
2021-09-18 15:51:09 +0530

    fix(data schema): Updated Schema columns and test ran table creations, insert. Removed BackRef for now

--- data/schema_init.py
+++ data/schema_init.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import (
     Column,
     DateTime,
@@ -15,11 +17,23 @@ Base = declarative_base()
 
 class URL(Base):
     __tablename__ = "url"
-    hash = Column(String, primary_key=True)
+    EncodedURL = Column(String, primary_key=True)
     OriginalURL = Column(String)
     CreationDate = Column(DateTime)
     ExpirationDate = Column(DateTime)
-    UserID = Column(Integer)
+    UserID = Column(Integer, ForeignKey("users.UserID"))
+
+    def __repr__(self):
+        return (
+            "<User(UserID='%s', EncodedURL='%s', OriginalURL='%s', CreationDate='%s', ExpirationDate='%s')>"
+            % (
+                self.UserID,
+                self.EncodedURL,
+                self.OriginalURL,
+                str(self.CreationDate),
+                str(self.ExpirationDate),
+            )
+        )
 
 
 class USERS(Base):
@@ -30,15 +44,50 @@ class USERS(Base):
     CreationDate = Column(DateTime)
     LastLogin = Column(DateTime)
 
-    url = relationship(
-        URL, backref=backref("users", uselist=True, cascade="delete,all")
+    def __repr__(self):
+        return (
+            "<User(UserID='%s', Name='%s', Email='%s', CreationDate='%s', LastLogin='%s')>"
+            % (
+                self.UserID,
+                self.Name,
+                self.Email,
+                str(self.CreationDate),
+                str(self.LastLogin),
+            )
+        )
+
+
+if __name__ == "__main__":
+
+    ## Creation of Tables
+    engine = create_engine("sqlite:///./data/TinyURL.sqlite")
+    session = sessionmaker()
+    session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    ## Insertions
+    session = session()
+    one_url = URL(
+        EncodedURL="asdfae2",
+        OriginalURL="https://www.hello.com",
+        CreationDate=datetime.today(),
+        ExpirationDate=datetime.today(),
+        UserID=1412,
     )
+    one_user = USERS(
+        UserID=1412,
+        Name="John Doe",
+        Email="john.doe@xmen.com",
+        CreationDate=datetime.today(),
+        LastLogin=datetime.now(),
+    )
+    # print(one_user)
+    session.add(one_user)
+    session.add(one_url)
+    session.commit()
 
+    for row in session.query(URL).order_by(URL.EncodedURL):
+        print(row)
 
-engine = create_engine("sqlite:///./data/TinyURL.sqlite")
-session = sessionmaker()
-session.configure(bind=engine)
-Base.metadata.create_all(engine)
-
-
-## use cassandra
+    for row in session.query(USERS).order_by(USERS.UserID):
+        print(row)
