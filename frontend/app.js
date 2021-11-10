8f2f0dca246757febb2beb817f32dfee653c9553
niksinfinity <nikeshinfinity@gmail.com>
2021-11-10 21:36:58 +0530

    fix

--- frontend/app.js
+++ frontend/app.js
@@ -51,7 +51,7 @@ function getLongURL(longurl) {
         }
         var encodedURL;
         backend_server_url = "https://www.cloned-link.com"
-        fetch(backend_server_url + "/v1/encode-url/", {
+        fetch(backend_server_url + "/v1/encode-url", {
             method: 'POST',
             mode:'cors',
             headers: {
