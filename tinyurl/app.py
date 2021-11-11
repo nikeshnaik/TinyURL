ea77e97aae95accaf1ce58c0296e4ca358429b72
niksinfinity <nikeshinfinity@gmail.com>
2021-11-11 15:19:57 +0530

    fix(allowed method): Added GET POST DELETE methods in fastapi app allowed method middleware

--- tinyurl/app.py
+++ tinyurl/app.py
@@ -46,7 +46,7 @@ app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["POST", "GET", "DELETE"],
+        allow_methods=["*"],
         allow_headers=["*"]
         )
 
